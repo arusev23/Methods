d8e5aceda624da80fb5b0fb452f9a4dc36765802
Alexander Rusev <alexanderrusev@abv.bg>
2018-06-04 16:13:06 +0300

    Adding Numbers Triangle

---
+++ numbersTriangle.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class numbersTriangle {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		
+		trianglesPrinter(n);
+	}
+	
+	static void trianglesPrinter(int number) {
+//		int reverse = number;
+		for(int i = 1; i <= number; i++) {
+			
+			for (int j = 1; j < i; j++) {
+				System.out.print(j + " ");
+			}
+			
+			System.out.print(i);
+			
+			System.out.println();
+			
+		}
+		
+		for (int i = (number - 1); i >= 1; i--) {
+			
+			for (int j = 1; j < i; j++) {
+				System.out.print(j + " ");
+			}
+			
+			System.out.print(i);
+			
+			System.out.println();
+			
+		}
+		
+	}
+}
