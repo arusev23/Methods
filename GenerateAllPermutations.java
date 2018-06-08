de60d325ff26805eb281caa6f5a0afde5e9dad81
Alexander Rusev <alexanderrusev@abv.bg>
2018-06-08 14:42:45 +0300

    Permutations still doesn't print correctly

--- GenerateAllPermutations.java
+++ GenerateAllPermutations.java
@@ -1,17 +1,37 @@
-public class GenerateAllPermutations {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-	static void permute(java.util.List<Integer> arr, int k) {
+public class GenerateAllPermutations {
+	static ArrayList<String> collection = new ArrayList<>();
+
+	static void permute(ArrayList<String> arr, int k) {
 		for (int i = k; i < arr.size(); i++) {
 			java.util.Collections.swap(arr, i, k);
 			permute(arr, k + 1);
 			java.util.Collections.swap(arr, k, i);
 		}
 		if (k == arr.size() - 1) {
-			System.out.println(java.util.Arrays.toString(arr.toArray()));
+			// java.util.Collections.sort(arr);
+			collection.add(arr.toString());
 		}
+		collection.sort(null);
+		// return collection;
+
 	}
 
 	public static void main(String[] args) {
-		permute(java.util.Arrays.asList(1, 2, 3, 4, 5), 0);
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+
+		ArrayList<String> input = new ArrayList<>();
+		for (int i = 1; i <= n; i++) {
+			input.add(Integer.toString(i));
+		}
+
+		permute(input, 0);
+		for (String lineTokens : collection) {
+			System.out.println(lineTokens);
+		}
+
 	}
 }
